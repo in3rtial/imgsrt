8b1e787705c75b57658838be81f64ebe791e69aa
gab <gab1000000000@hotmail.com>
2014-07-04 18:16:52 -0400

    linted the script

--- src/pixel_sorting.py
+++ src/pixel_sorting.py
@@ -1,29 +1,28 @@
 """transliteration of Kim Asendorf's pixel sorting script"""
 
-from numpy import int32
 from copy import copy
 from random import random, gauss
 from PIL import Image
-
+from numpy import int32
 
 # PROGRAM CONSTANTS
-black_value = int32(-10000000)  # rgb(103, 105, 128)
-white_value = int32(-6000000)  # rgb(164, 114, 128)
-brightness_value = 30  # 255 << 24) + (32<< 16) + (32 << 8) + 32
-white_value = int32((255 << 24) + (230<< 16) + (230 << 8) + 230)
+
+# rgb(103, 105, 128)
+BLACK_VALUE = int32(-10000000)
+# rgb(164, 114, 128)
+WHITE_VALUE = int32((255 << 24) + (230 << 16) + (230 << 8) + 230)
+BRIGHTNESS_VALUE = 30  # 255 << 24) + (32<< 16) + (32 << 8) + 32
 
 
-
-#PIXEL CONVERSION FUNCTIONS
+# PIXEL CONVERSION FUNCTIONS
 def get_pixel_value(pixel):
     """rgb pixel to int32 processing representation"""
-    return(int32(( ((255 << 8) | pixel[0]) << 8 | pixel[1]) << 8 | pixel[2]))
+    return(int32((((255 << 8) | pixel[0]) << 8 | pixel[1]) << 8 | pixel[2]))
 
 
 def get_pixel_brightness(pixel):
     """rgb pixel to brightness value"""
     return(max((pixel[0], pixel[1], pixel[2])) / 255 * 100)
-
 
 
 # PIXEL FINDING FUNCTIONS
@@ -37,69 +36,69 @@ def get_next_satisfying(vector, starting_position, condition_fun):
         position += 1
     if(position == (len(vector) - 1) and
        not(condition_fun(vector[position]))):
-        position = -1
+        position = - 1
     return(position)
 
 
-# black
+# black mode
 def get_next_black(vector, starting_position):
     """next black pixel"""
-    condition = lambda x: get_pixel_value(x) > black_value
+    condition = lambda x: int32(get_pixel_value(x)) > BLACK_VALUE
     return get_next_satisfying(vector, starting_position, condition)
 
 
 def get_next_not_black(vector, starting_position):
     """next non black pixel"""
-    condition = lambda x: get_pixel_value(x) < black_value
+    condition = lambda x: int32(get_pixel_value(x)) < BLACK_VALUE
     return get_next_satisfying(vector, starting_position, condition)
 
 
-# bright
+# bright mode
 def get_next_bright(vector, starting_position):
     """next bright pixel"""
-    condition = lambda x: get_pixel_brightness(x) < brightness_value
+    condition = lambda x: int32(get_pixel_brightness(x)) < BRIGHTNESS_VALUE
     return get_next_satisfying(vector, starting_position, condition)
 
 
 def get_next_dark(vector, starting_position):
     """next dark pixel"""
-    condition = lambda x: get_pixel_brightness(x) > brightness_value
+    condition = lambda x: int32(get_pixel_brightness(x)) > BRIGHTNESS_VALUE
     return get_next_satisfying(vector, starting_position, condition)
 
 
-#white
+# white mode
 def get_next_white(vector, starting_position):
     """next white pixel"""
-    condition = lambda x: get_pixel_value(x) < white_value
+    condition = lambda x: int32(get_pixel_value(x)) < WHITE_VALUE
     return get_next_satisfying(vector, starting_position, condition)
 
 
 def get_next_not_white(vector, starting_position):
     """next not white pixel"""
-    condition = lambda x: get_pixel_value(x) > white_value
+    condition = lambda x: int32(get_pixel_value(x)) > WHITE_VALUE
     return get_next_satisfying(vector, starting_position, condition)
 
 
-finding_functions = ((get_next_black, get_next_not_black),  # black
-                     (get_next_bright, get_next_dark),      # bright
-                     (get_next_white, get_next_not_white))  # white
-
+FIND_FUNCTIONS = ((get_next_black, get_next_not_black),  # black
+                  (get_next_bright, get_next_dark),      # bright
+                  (get_next_white, get_next_not_white))  # white
 
 
 # PIXEL SORTING FUNCTIONS
-def sort_pixels(vector, mode=0, finding_functions=finding_functions):
+def sort_pixels(vector, mode=0, find=FIND_FUNCTIONS):
     """sort pixel in the given vector"""
     assert(mode in (0, 1, 2)), "invalid use case"
     vector = copy(vector)
     position = 0
-    position_end = None
+    pos_end = None
     while(position < len(vector)):
-        if((position == -1) or (position_end == -1)):
+        if((position == -1) or (pos_end == -1)):
             break
-        position = finding_functions[mode][0](vector, position)
-        position_end = finding_functions[mode][1](vector, position)
-        vector[position:position_end] = sorted(vector[position:position_end], key=lambda x: get_pixel_value(x))
-        position = position_end + 1
+        position = find[mode][0](vector, position)
+        pos_end = find[mode][1](vector, position)
+        vector[position:pos_end] = sorted(vector[position:pos_end],
+                                          key=lambda x: get_pixel_value(x))
+        position = pos_end + 1
     return(vector)
 
 
@@ -107,7 +106,7 @@ def sort_pixels(vector, mode=0, finding_functions=finding_functions):
 def to_vectors(rgb_image, row_or_col):
     """rgb image -> list of lists of RGB tuples"""
     assert(rgb_image.mode == "RGB"), "must be a RGB image"""
-    assert(row_or_col in (0,1)), "row = 0, col = 1"
+    assert(row_or_col in (0, 1)), "row = 0, col = 1"
     vectors = []
     x_size, y_size = rgb_image.size
 
@@ -126,12 +125,13 @@ def to_vectors(rgb_image, row_or_col):
     return(vectors)
 
 
-
 # COMPLETE FUNCTIONS
-def sort_image(image, row_or_col, mode=0, prob=1, mu=1):
-    """ """
+def sort_image(image, row_or_col, mode=0, prob=1, avg_band_size=1):
+    """input: (rgb image, row or column, sort mode, probability of sorting,
+    average band size for sorting)
+    output: sorted out image)"""
     x_size, y_size = image.size
-    sigma = mu / 4
+    sigma = avg_band_size / 4
 
     vectors = to_vectors(image, row_or_col)
 
@@ -142,7 +142,7 @@ def sort_image(image, row_or_col, mode=0, prob=1, mu=1):
         if(random() < prob):
             # calculate the indices of the rows to sort
             to_sort = []
-            coarseness = int(gauss(mu, sigma))
+            coarseness = int(gauss(avg_band_size, sigma))
             for index in range(position, position + coarseness):
                 if(index >= len(vectors)):
                     break
@@ -159,19 +159,18 @@ def sort_image(image, row_or_col, mode=0, prob=1, mu=1):
     new_image = []
     if(row_or_col == 0):
         for vector in new_vectors:
-            for (R,G,B) in vector:
-                new_image.append(int(R))
-                new_image.append(int(G))
-                new_image.append(int(B))
+            for (red, green, blue) in vector:
+                new_image.append(int(red))
+                new_image.append(int(green))
+                new_image.append(int(blue))
     else:
         for i in range(0, y_size):
             for vector in new_vectors:
-                (R, G, B) = vector[i]
-                new_image.append(int(R))
-                new_image.append(int(G))
-                new_image.append(int(B))
+                (red, green, blue) = vector[i]
+                new_image.append(int(red))
+                new_image.append(int(green))
+                new_image.append(int(blue))
     return(Image.fromstring('RGB', (x_size, y_size), bytes(new_image)))
 
 
-
-__all__ = ["sort_image"]
+__all__ = ["Image", "sort_image"]
